64714543bd1c903aa0e19ea8ac470ea16835e0c6
dee3-pika <146899185+dee3-pika@users.noreply.github.com>
2024-06-26 21:25:19 +0545

    fourth commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useContext } from "react";
+import { Context } from "../../main";
+import { Navigate } from "react-router-dom";
+import HeroSection from "./HeroSection";
+import HowItWorks from "./HowItWorks";
+import PopularCategories from "./PopularCategories";
+import PopularCompanies from "./PopularCompanies";
+import Navbar from "../Layout/Navbar";
+
+const Home = () => {
+  const { isAuthorized } = useContext(Context);
+  if (!isAuthorized) {
+    return <Navigate to={"/login"} />;
+  }
+ 
+ 
+ 
+  
+  return (
+    <>
+      <section className="homePage page">
+        <Navbar/>
+        <HeroSection />
+        <HowItWorks />
+        <PopularCategories />
+        <PopularCompanies />
+      </section>
+    </>
+  );
+};
+
+export default Home;
